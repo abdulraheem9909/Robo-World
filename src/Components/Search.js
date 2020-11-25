3904a6190f7577d538cebddd24b7baabf7e4d30e
abdulraheem9909 <abdulraheem9909@gmail.com>
2020-11-25 21:00:14 +0500

    Updates

--- src/Components/Search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
- const Search =(props)=>{
-     return (
-         <div className=" pa2" >
-         <input className="pa3 ba b--light-silver bg-lightest-blue" type='search' placeholder='Enter the name' onChange={props.searched}/>
-         </div>
-     )
- }
- export default Search;
